8dc5bc64259701030b32315565e800b4a64c1791
Joy Christine <joychristin2@gmail.com>
2020-12-08 17:10:22 +0300

    initial commit

---
+++ models/category.js
@@ -0,0 +1,19 @@
+ var mongoose = require('mongoose');
+
+ var Schema = mongoose.Schema;
+
+ var CategorySchema = new Schema({
+     name: { type: String, required: true, maxlength: 100 },
+     description: String,
+
+ });
+
+ // Virtual for category's URL
+ CategorySchema
+     .virtual('url')
+     .get(function() {
+         return '/inventory-app/category/' + this._id;
+     });
+
+ //Export model
+ module.exports = mongoose.model('Category', CategorySchema);
